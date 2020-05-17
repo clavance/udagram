292a545d33768d39770b90e3cc74cdbdc9954780
Clavance Lim <clavance@outlook.com>
2020-05-17 02:16:33 +0100

    feature: add auth functions

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -36,25 +36,29 @@ function generateJWT(user: User): string {
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
+    // check that there is a request header, with authorisation header
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
 
+    //split authorisation header after 'Bearer: '
+    const token_bearer = req.headers.authorization.split(' ');
+    // sanity check
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
+    // second half of the split string
+    const token = token_bearer[1];
 
-    // const token = token_bearer[1];
-
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    // verify that the token is legitimate, by decrypting using the same jwt_secret key
+    // if successful, pass back to caller
+    return jwt.verify(token, config.jwt_secret, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
 }
 
 router.get('/verification',
